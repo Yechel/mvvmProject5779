28de6034ace4093e0f23c3916e2f0d133122f384
yechiel chabany <Yechel@users.noreply.github.com>
2019-04-24 14:10:30 +0300

    Add project files.

---
+++ FL_Project/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FL_Project.Model;
+
+namespace FL_Project.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
